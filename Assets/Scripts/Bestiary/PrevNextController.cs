6937b881579e4de39c311ebe102a7c9e61928cc1
Guilherme <guilhermecmartins99@gmail.com>
2021-09-01 17:48:56 -0300

    Criada a paginacao do bestiario

---
+++ Assets/Scripts/Bestiary/PrevNextController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PrevNextController : MonoBehaviour
+{
+    //Serialized Fields
+    [Header("Pages configuration")]
+    [SerializeField]
+    private GameObject[] pages = new GameObject[3];
+
+    //Non Serialized Fields
+    private int countPages = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.pages[0].SetActive(true);
+        this.pages[1].SetActive(false);
+        this.pages[2].SetActive(false);
+    }
+
+    public void nextPage()
+    {
+        if(this.countPages < 2)
+        {
+            this.pages[this.countPages].SetActive(false);
+            this.countPages++;
+            this.pages[this.countPages].SetActive(true);
+        }
+    }
+
+    public void prevPage()
+    {
+        if (this.countPages > 0)
+        {
+            this.pages[this.countPages].SetActive(false);
+            this.countPages--;
+            this.pages[this.countPages].SetActive(true);
+        }
+    }
+}
---
+++ Assets/Scripts/GameController/BestiaryController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BestiaryController : MonoBehaviour
+{
+    //Serialized Fields
+    [Header("Inventory configuration")]
+    [SerializeField]
+    private GameObject bestiary = null;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.bestiary.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        openCloseInventory();
+    }
+
+    private void openCloseInventory()
+    {
+        if (Input.GetKeyDown(KeyCode.B))
+        {
+            bool isOpen = !this.bestiary.activeSelf;
+            this.bestiary.SetActive(isOpen);
+
+            if (isOpen)
+            {
+                Time.timeScale = 0;
+            }
+            else
+            {
+                Time.timeScale = 1;
+            }
+        }
+    }
+}
