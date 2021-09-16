321ebe970f2b117aa5e5ad983aa727be4a667f86
Guilherme <guilhermecmartins99@gmail.com>
2021-09-16 17:16:51 -0300

    Refatorado a mecanica de ataque dos monstros

---
+++ Assets/Scripts/Monsters/AtackController/AtackAnimationTriggers.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AtackAnimationTriggers : MonoBehaviour
+{
+
+    [Header("Projectile configuration")]
+    [SerializeField]
+    private GameObject projectile = null;
+
+    public void turnAtackColliderOn()
+    {
+        this.gameObject.transform.GetChild(0).gameObject.SetActive(true);
+    }
+
+    public void turnAtackColliderOff()
+    {
+        this.gameObject.transform.GetChild(0).gameObject.SetActive(false);
+    }
+
+    public void atackRange()
+    {
+        Instantiate(this.projectile, transform.position, Quaternion.identity);
+    }
+}
--- Assets/Scripts/Monsters/AtackController/AtackController.cs
+++ Assets/Scripts/Monsters/AtackController/AtackController.cs
@@ -9,31 +9,20 @@ public class AtackController : MonoBehaviour
     [Header("Animator Configuration")]
     [SerializeField]
     private Animator animator = null;
-    [Header("Player configuration")]
-    [SerializeField]
-    private GameObject player = null;
-    [Header("Projectile configuration")]
-    [SerializeField]
-    private GameObject projectile = null;
 
-    // Update is called once per frame
-    void Update()
+    void onTriggerEnter2D(Collider2D collider)
     {
-        
+        if (collider.tag == "Player")
+        {
+            this.animator.SetBool("IsAtacking", true);
+        }
     }
 
     void OnTriggerStay2D(Collider2D collider)
     {
         if (collider.tag == "Player")
         {
-            if (Math.Abs(this.gameObject.transform.position.x - this.player.transform.position.x) < 0.1 && Math.Abs(this.gameObject.transform.position.y - this.player.transform.position.y) < 0.3)
-            {
-                this.animator.SetBool("IsAtacking", true);
-            }
-            else
-            {
-                this.animator.SetBool("IsAtacking", false);
-            }
+            this.animator.SetBool("IsAtacking", true);
         }
     }
 
@@ -43,20 +32,5 @@ public class AtackController : MonoBehaviour
         {
             this.animator.SetBool("IsAtacking", false);
         }
-    }
-
-    public void turnAtackColliderOn()
-    {
-        this.gameObject.transform.GetChild(0).gameObject.SetActive(true);
-    }
-
-    public void turnAtackColliderOff()
-    {
-        this.gameObject.transform.GetChild(0).gameObject.SetActive(false);
-    }
-
-    public void atackRange()
-    {
-        Instantiate(this.projectile, transform.position, Quaternion.identity);
     }
 }
