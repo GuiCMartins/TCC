e04e969e8f8061ac56fbd67ba8511f3539d0af69
Guilherme <guilhermecmartins99@gmail.com>
2021-09-16 15:19:03 -0300

    Feito o segundo puzzle da dark wood

---
+++ Assets/Scripts/InteractionObjects/InteractioKey/DarkWoodKey/InteractionDarkWoodKey.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractionDarkWoodKey : Interaction
+{
+    public override void interaction()
+    {
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            Debug.Log("Open the door!");
+            Destroy(this.gameObject);
+        }
+    }
+}
