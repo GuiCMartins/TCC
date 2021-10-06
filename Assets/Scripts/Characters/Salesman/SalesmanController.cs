9b53164567ec689e7a628caec78c0e348e7a1b25
Guilherme <guilhermecmartins@gmail.com>
2021-10-06 17:49:18 -0300

    Criacao dos dialogos da primeira fase

--- Assets/Scripts/Characters/Salesman/SalesmanController.cs
+++ Assets/Scripts/Characters/Salesman/SalesmanController.cs
@@ -8,11 +8,19 @@ public class SalesmanController : Interaction
     [Header("Store configuration")]
     [SerializeField]
     private GameObject store = null;
+    [Header("Tutorial quest configuration")]
+    [SerializeField]
+    private QuestBase quest = null;
 
     public override void interaction()
     {
         if (Input.GetKeyDown(KeyCode.F))
         {
+            if (this.quest.getCurrentDialogId() == 3)
+            {
+                this.quest.setCurrentDialogId(4);
+            }
+
             this.store.SetActive(true);
             Time.timeScale = 0;
         }   
---
+++ Assets/Scripts/Quests/Fase1/Fase1Quest.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fase1Quest : QuestBase
+{
+    //Serialized fields
+    [Header("Monsters configuration")]
+    [SerializeField]
+    private List<GameObject> monsters = new List<GameObject>();
+
+    //Non serialized fields
+    private bool isAllMonterDead = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!this.isAllMonterDead && verifyMonsterDead())
+        {
+            this.isAllMonterDead = true;
+            setCurrentDialogId(0);
+            base.executeCurrentDialog();
+        }
+    }
+
+    public override void setCurrentDialogId(int id)
+    {
+        base.setBaseCurrentDialogId(id);
+    }
+
+    public override void setOtherCharacterDialogId(int id)
+    {
+        base.setBaseOtherCharacterDialogId(id);
+    }
+
+    private bool verifyMonsterDead()
+    {
+        foreach (GameObject monster in  this.monsters)
+        {
+            if (monster != null)
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
