85c3a2ad4a0caee415fcd353e6468cac28184777
Guilherme <guilhermecmartins@gmail.com>
2021-10-12 16:45:38 -0300

    Feito objetos darem dano

---
+++ Assets/Scripts/DamageObject/DamageObject.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DamageObject : MonoBehaviour
+{
+    [Header("Damage configuration")]
+    [SerializeField]
+    private int damage = 20;
+
+    void OnTriggerEnter2D(Collider2D collider)
+    {
+        if (collider.tag == "Player")
+        {
+            collider.gameObject.SendMessage("takeDamage", this.damage);
+        }
+    }
+}
