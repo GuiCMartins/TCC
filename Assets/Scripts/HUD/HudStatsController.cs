15ddea655031a1f8b4e74b064967b02a2b0b2d50
Guilherme <guilhermecmartins@gmail.com>
2021-10-14 14:49:46 -0300

    Habilitacao e desabilitacao do painel de status

---
+++ Assets/Scripts/HUD/HudStatsController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HudStatsController : MonoBehaviour
+{
+    //Serialized Fields
+    [Header("Hud Stats Configuration")]
+    [SerializeField]
+    private GameObject stats = null;
+    [Header("Hud closed stats Configuration")]
+    [SerializeField]
+    private GameObject closedStats = null;
+
+    public void closeStats()
+    {
+        this.stats.SetActive(false);
+        this.closedStats.SetActive(true);
+    }
+
+    public void openStats()
+    {
+        this.closedStats.SetActive(false);
+        this.stats.SetActive(true);
+    }
+}
