33cf3fc26d3752e1179cea94495d14f535b04ed8
Guilherme <guilhermecmartins@gmail.com>
2021-10-16 16:37:13 -0300

    Esc

---
+++ Assets/Scripts/HUD/Esc.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Esc : MonoBehaviour
+{
+    //Serialized Fields
+    [Header("Esc configuration")]
+    [SerializeField]
+    private GameObject esc = null;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.esc.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        openEsc();
+    }
+
+    private void openEsc()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            this.esc.SetActive(true);
+            Time.timeScale = 0;
+        }
+    }
+}
